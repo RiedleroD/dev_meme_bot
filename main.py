9683c4d48fca7f236564d8cfe291dc8427e1105b
RiedleroD <riedler@gelse.eu>
2021-09-16 13:11:12 +0200

    fixed exception when trying to mention user without username

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from sys import argv
 from datetime import datetime
 import json
 
-from telegram import Update
+from telegram import Update, ParseMode
 from telegram.ext import Updater, CommandHandler, MessageHandler, CallbackContext
 from telegram.ext.filters import Filters
 
@@ -33,17 +33,24 @@ def ping(update: Update, context: CallbackContext):
 	dt=datetime.now(update.message.date.tzinfo)-update.message.date
 	update.message.reply_text(f'Ping is {dt.total_seconds():.2f}s')
 
+def get_mention(user):
+	if user.username == None:
+		return f"[{user.first_name}](tg://user?id={user.id})"
+	else:
+		return "@"+user.username
+
 @message(Filters.status_update.new_chat_members)
 def new_chat_member(update: Update, context: CallbackContext):
-	handles=", ".join(user.username for user in update.message.new_chat_members)
+	handles=", ".join(get_mention(user) for user in update.message.new_chat_members)
 	update.message.reply_text(
 f"""{handles},
-いらっしゃいませ! [Welcome!]
+いらっしゃいませ! \\[Welcome!\\]
 Welcome to this chat! Please read the rules.
 Добро пожаловать в чат! Прочти правила, пожалуйста.
 このチャットへようこそ！ ルールをお読みください。
 
-https://t.me/dev_meme/3667""")
+[rules](https://t.me/dev_meme/3667)""",
+parse_mode=ParseMode.MARKDOWN)
 
 print("starting polling")
 updater.start_polling()
