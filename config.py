f5aafb2e4d531c3970414d04e4d5187d69ca5a14
Riedler <git@riedler.wien>
2025-06-05 19:51:56 +0200

    fall back to default config if necessary

--- config.py
+++ config.py
@@ -13,24 +13,30 @@ class Config(TypedDict):
 	spam_threshhold: int
 
 
+defaultconfig: Config = {
+	'token': 'Your token goes here',
+	'private_chat_id': -1001218939335,
+	'private_chat_username': 'devs_chat',
+	'database_path': 'memebot.db',
+	'message_memory': 100,
+	'spam_threshhold': 2,
+}
+
 print("reading config")
 CURDIR = path.dirname(sys.argv[0])
 CONFPATH = path.join(CURDIR, 'config.json')
 if not path.exists(CONFPATH):
 	with open(CONFPATH, 'w') as f:
-		config = {
-			'token': 'Your token goes here',
-			'private_chat_id': -1001218939335,
-			'private_chat_username': 'devs_chat',
-			'database_path': 'memebot.db',
-			'message_memory': 100,
-			'spam_threshhold': 2,
-		}
-		json.dump(config, f, indent=2)
+		json.dump(defaultconfig, f, indent=2)
 		print(f'Config was created in {CONFPATH}, please edit it')
 		sys.exit(0)
 
 with open(CONFPATH) as f:
 	CONFIG: Config = json.load(f)
+
+for k, v in defaultconfig.items():
+	if k not in CONFIG.keys():
+		CONFIG[k] = v  # type: ignore[literal-required]
+		print(f"  defaulting to {k}={v}")
 
 CONFIG['database_path'] = path.join(CURDIR, CONFIG['database_path'])
