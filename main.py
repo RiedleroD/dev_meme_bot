75ee891cfb7e206d5bdb96a0b0fb54c34fb0b3a2
zyrafal <37906205+zyrafal@users.noreply.github.com>
2025-07-16 19:19:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import asyncio
 from math import floor, log10
 from datetime import datetime
 from collections.abc import Callable
@@ -23,12 +24,13 @@ db = database.UserDB(CONFIG['database_path'])
 print("initializing commands")
 application = Application.builder().token(CONFIG["token"]).build()
 
-async def remove_vk_messages(context):
-	msgs = db.cleanup_votekicks()
+async def delete_vk_messages(context):
+	db.cleanup_votekicks()
+	msgs = db.get_expired_messages()
 	if msgs:
 		await context.bot.delete_messages(private_chat_id, msgs)
 
-application.job_queue.run_repeating(remove_vk_messages, interval=60, first=0)
+application.job_queue.run_repeating(delete_vk_messages, interval=60, first=0)
 
 
 def on_command(name: str) -> Callable[[Callable], Callable]:
@@ -229,44 +231,47 @@ async def votekick(update: Update, context: CallbackContext):
 	assert voter is not None
 	assert chat is not None
 
-	if tuser.id == 777000:
-		if (db.get_trusted(voter.id) or await is_admin(chat, voter)):
-			reply = await update.message.reply_text(
-				"You can't votekick the channel…",
-				parse_mode=ParseMode.MARKDOWN_V2
-			)
-			db.add_vk_messages(tuser.id, [update.message.message_id, reply.message_id])
-		else:
-			await update.message.delete()
-	elif not (db.get_trusted(voter.id) or await is_admin(chat, voter)):
+	if not (db.get_trusted(voter.id) or await is_admin(chat, voter)):
 		reply = await update.message.reply_text(
 			'Only trusted users can votekick someone',
 			parse_mode=ParseMode.MARKDOWN_V2
 		)
-		db.add_vk_messages(tuser.id, [update.message.message_id, reply.message_id])
+		await asyncio.sleep(5)
+		await context.bot.delete_messages(chat.id, [update.message.message_id, reply.message_id])
+	elif tuser.id == 777000:
+		reply = await update.message.reply_text(
+			'You can\'t votekick the channel…',
+			parse_mode=ParseMode.MARKDOWN_V2
+		)
+		await asyncio.sleep(5)
+		await context.bot.delete_messages(chat.id, [update.message.message_id, reply.message_id])
 	elif db.get_trusted(tuser.id):
 		reply = await update.message.reply_text(
 			'You can\'t votekick another trusted user',
 			parse_mode=ParseMode.MARKDOWN_V2
 		)
-		db.add_vk_messages(tuser.id, [update.message.message_id, reply.message_id])
+		await asyncio.sleep(5)
+		await context.bot.delete_messages(chat.id, [update.message.message_id, reply.message_id])
 	elif await is_admin(chat, tuser):
 		reply = await update.message.reply_text(
 			'You can\'t votekick an admin',
 			parse_mode=ParseMode.MARKDOWN_V2
 		)
-		db.add_vk_messages(tuser.id, [update.message.message_id, reply.message_id])
+		await asyncio.sleep(5)
+		await context.bot.delete_messages(chat.id, [update.message.message_id, reply.message_id])
 	else:
+		votes_required = 1
+		
 		db.add_votekick(voter.id, tuser.id)
 		votes = db.get_votekicks(tuser.id)
 		votec = len(votes)
-		appendix = "\nthat constitutes a ban\\!" if votec >= 3 else ""
+		appendix = "\nthat constitutes a ban\\!" if votec >= votes_required else ""
 		reply = await update.message.reply_text(
-			f'User {get_mention(tuser)} now has {votec}/3 votes against them\\.{appendix}',
+			f'User {get_mention(tuser)} now has {votec}/{votes_required} votes against them\\.{appendix}',
 			parse_mode=ParseMode.MARKDOWN_V2
 		)
 		
-		if votec >= 3:
+		if votec >= votes_required:
 			# don't remove the bot's final message
 			db.add_vk_messages(tuser.id, [update.message.message_id])
 
@@ -275,8 +280,6 @@ async def votekick(update: Update, context: CallbackContext):
 			# award score to all eligible users
 			for userid in votes:
 				db.increment_vkscore(userid)
-
-			db.end_votekick(tuser.id)
 		else:
 			db.add_vk_messages(tuser.id, [update.message.message_id, reply.message_id])
 
